45c59705f370f7fbe260e27c0aca7709f328812b
Pavel Kapitonov <161024801+Pavel-Kapitonov@users.noreply.github.com>
2026-04-26 18:18:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -28,12 +28,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const statusMsg = document.getElementById('statusMessage');
 
     form.addEventListener('submit', function(e) {
-        e.preventDefault(); // Чтобы страница не перезагружалась
+        e.preventDefault(); 
 
-        // Собираем данные
+
         const formData = new FormData(form);
 
-        // Отправляем
+
         fetch('', {
             method: 'POST',
             body: formData,
@@ -43,15 +43,14 @@ document.addEventListener('DOMContentLoaded', function() {
             if (response.ok) {
                 statusMsg.textContent = '✅ Заявка успешно отправлена!';
                 statusMsg.className = 'status success';
-                form.reset(); // Очистить поля
+                form.reset();
             } else {
-                // Если ошибка
+
                 statusMsg.textContent = '❌ Ошибка отправки.';
                 statusMsg.className = 'status error';
             }
         })
         .catch(function(error) {
-            // Если совсем всё плохо (например, нет интернета)
             statusMsg.textContent = '❌ Ошибка отправки.';
             statusMsg.className = 'status error';
         });
@@ -66,7 +65,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 $(document).ready(function(){
 
-    // 1. Находим наш слайдер и сохраняем в переменную
     var $slider = $('.cottage-slider');
 
     $slider.slick({
@@ -79,24 +77,18 @@ $(document).ready(function(){
         nextArrow: $('.next-slide')
     });
 
-    // 3. Узнаем, сколько всего слайдов
     var totalSlides = $slider.slick('getSlick').slideCount;
 
-    // Если число меньше 10, добавляем нолик (было 6 - стало 06)
     if (totalSlides < 10) {
         $('.total').text('0' + totalSlides);
     } else {
         $('.total').text(totalSlides);
     }
 
-    // Каждый раз, когда слайд сменился, запускается эта функция
     $slider.on('afterChange', function(event, slick, currentSlide){
         
-        // currentSlide — это номер слайда, начиная с 0 (0, 1, 2...)
-        // Нам нужно привычное число, поэтому прибавляем 1
         var slideNumber = currentSlide + 1;
 
-        // Опять проверка: если меньше 10, добавляем "0" в начало
         if (slideNumber < 10) {
             $('.current').text('0' + slideNumber);
         } else {
